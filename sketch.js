b611f2ee6b689690e04423831b535c1215e2dfb0
Thomas Garcia <144739053+epicsharkbait8@users.noreply.github.com>
2025-09-17 17:47:17 +0000

    feat: Setting up game in p5.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 400);
+    background(220);
+}
+
+function draw() {
+    //Game loop will go here
+    FileList(255, 0, 0);
+    ellipse(50, 50, 20, 20);
+
+    //Display game title type
+    fill(0);
+    textSize(24);
+    textAlign(CENTER);
+    Text("Marble Maze - coming soon!", width/2, height/2)
+}
